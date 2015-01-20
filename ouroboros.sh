9a1c7cd71333623a3a2af0db3d59aafb2fd4bcf0
Tim Cameron Ryan <id@timryan.org>
2015-01-19 20:24:24 -0800

    Adds ouroboros self-test.

---
+++ ouroboros.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+trap '[ -n "$(jobs -pr)" ] && kill $(jobs -pr)' INT QUIT TERM EXIT
+
+cd agent
+source venv/bin/activate
+python3 src/agent.py &
+AGENT_PID=$!
+
+cd ../remote; node src/remote.js tusk
+SUCCESS=$?
+
+# sleep 5
+
+isalive() {
+	kill -0 "$1" >/dev/null 2>&1
+	return $?
+}
+
+kill "$AGENT_PID" >/dev/null 2>&1
+while isalive "$AGENT_PID"; do
+    sleep 0.5
+done
+
+echo "[integration test finished with $SUCCESS]"
+exit $SUCCESS
