3042b6b15b9e0326f3667aca1b8e0c143f7e3ae0
Tim Cameron Ryan <id@timryan.org>
2015-02-02 22:42:19 -0800

    Simplifies gc storage code. Adds bitbucket key.

--- src/check.js
+++ src/check.js
@@ -1,62 +1,57 @@
 var spawn = require('child_process').spawn;
+var Promise = require('bluebird');
+
 var util = require('./util');
 var config = require('./config');
 
-// Check if a ref is already compiled.
-function check (ref, next) {
+function gc_storage (object, mode, next) {
   var conf = config.read();
 
-  var p = spawn("ansible", ["all",
-    "-i", "localhost,",
-    "-c", "local",
-    "-m", "gc_storage",
-    "-a",
-      "bucket=tusk " +
-      // "object=9b934cf284d9b196f48b43876d3e01912797242c.tar.gz " +
-      "object=" + util.refSha(ref) + ".tar.gz " +
-      "mode=get_url " +
-      "gs_access_key=" + conf.gstorage.key + " " +
-      "gs_secret_key=" + conf.gstorage.secret + " "]);
-  util.collect(p, function (err, data) {
-    var result, err = null;
-    try {
-      result = JSON.parse(String(data).replace(/^.*>>\s*/g, ''));
-      if (!result.failed) {
-        return next(null, result.url);
+  return new Promise(function (resolve, reject) {
+    var p = spawn("ansible", ["all",
+      "-i", "localhost,",
+      "-c", "local",
+      "-m", "gc_storage",
+      "-a",
+        "bucket=tusk " +
+        // "object=9b934cf284d9b196f48b43876d3e01912797242c.tar.gz " +
+        "object=" + object + " " +
+        "mode=" + mode + " " +
+        "gs_access_key=" + conf.gstorage.key + " " +
+        "gs_secret_key=" + conf.gstorage.secret + " "]);
+
+    util.collect(p, function (err, data) {
+      var result;
+      try {
+        result = JSON.parse(String(data).replace(/^.*>>\s*/g, ''));
+        if (!result.failed) {
+          return resolve(result);
+        }
+      } catch (err) {
+        return reject(err);
       }
-    } catch (e) {
-      err = e;
-    }
-    return next(err || (result ? new Error(result.msg) : new Error("Unknown error")));
+      if (result) {
+        reject(new Error(result.msg));
+      } else {
+        reject(new Error("Unknown error"));
+      }
+    })
   })
+  .nodeify(next);
+}
+
+// Check if a ref is already compiled.
+function check (ref, next) {
+  return gc_storage(util.refSha(ref) + ".tar.gz", "get_url")
+    .then(function (result) {
+      return result.url;
+    })
+    .nodeify(next);
 }
 
 function destroy (ref, next) {
-  var conf = config.read();
-
-  var p = spawn("ansible", ["all",
-    "-i", "localhost,",
-    "-c", "local",
-    "-m", "gc_storage",
-    "-a",
-      "bucket=tusk " +
-      // "object=9b934cf284d9b196f48b43876d3e01912797242c.tar.gz " +
-      "object=" + util.refSha(ref) + ".tar.gz " +
-      "mode=delete " +
-      "gs_access_key=" + conf.gstorage.key + " " +
-      "gs_secret_key=" + conf.gstorage.secret + " "]);
-  util.collect(p, function (err, data) {
-    var result, err = null;
-    try {
-      result = JSON.parse(String(data).replace(/^.*>>\s*/g, ''));
-      if (!result.failed) {
-        return next(null, result);
-      }
-    } catch (e) {
-      err = e;
-    }
-    return next(err || (result ? new Error(result.msg) : new Error("Unknown error")));
-  })
+  return gc_storage(util.refSha(ref) + ".tar.gz", "delete")
+    .nodeify(next);
 }
 
 exports.check = check;
